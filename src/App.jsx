399bb66ddabe1cfe2f55faaa16bb5e87a11715a4
bayuadam <bayuadam834@gmail.com>
2023-08-02 16:49:30 +0700

    add feature delete todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Todos from "./components/Todos";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -7,20 +8,24 @@ function App() {
       id: 1,
       title: "Belajar Web Development",
       completed: true,
+      deleted: false,
     },
     {
       id: 2,
       title: "Bermain Futsal",
       completed: false,
+      deleted: false,
     },
     {
       id: 3,
       title: "Belajar Mandiri",
       completed: false,
+      deleted: false,
     },
   ]);
 
   const toggleCompleted = (todoId) => {
+    // console.log(todoId, );
     const updatedTodos = todos.map((todo) => {
       if (todo.id === todoId) {
         todo.completed = !todo.completed;
@@ -30,11 +35,24 @@ function App() {
     setTodos(updatedTodos);
   };
 
+  const deleteTodo = (todoId) => {
+    const deleteTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.deleted = true;
+      }
+      return todo;
+    });
+    setTodos(deleteTodos);
+  };
+
   return (
-    //  CSS
     <div style={{ textAlign: "center", padding: "12px" }}>
       <h1 style={{ fontSize: "36px" }}>My Todo List</h1>
-      <Todos todos={todos} toggleCompleted={toggleCompleted} />
+      <Todos
+        todos={todos}
+        toggleCompleted={toggleCompleted}
+        toggleDeleted={deleteTodo}
+      />
     </div>
   );
 }
